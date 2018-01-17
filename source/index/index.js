c1d018de2042a95edd7aaef52f7ad10806a363b5
ismagilov <ismagilov.job@yandex.ru>
2018-01-17 13:00:34 +0300

    Source and Build folders deleted

--- source/index/index.js
+++
@@ -1,6 +0,0 @@
-import './index.scss';
-import 'normalize.css';
-
-import createMenu from '../components/menu/menu';
-var menu = createMenu(['Home', 'О решении', 'Преимущества', 'Бонусы', 'Контакты'], 'menu');
-document.body.appendChild(menu);
